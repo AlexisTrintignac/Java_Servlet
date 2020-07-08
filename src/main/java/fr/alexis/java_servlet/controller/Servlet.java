9b37a192e5686852265397341494c1c4db048d63
Alexis Trintignac <alexis.trintignac@gmail.com>
2020-07-08 12:02:44 +0200

    init projetc servlet

---
+++ src/main/java/fr/alexis/java_servlet/controller/Servlet.java
@@ -0,0 +1,20 @@
+package fr.alexis.java_servlet.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Servlet extends HttpServlet {
+
+    private static final String TEXT_MESSAGE = "TEST MESSAGE";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>" + TEXT_MESSAGE + "</h1>");
+    }
+}
